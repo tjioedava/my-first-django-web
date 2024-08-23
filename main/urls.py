3a6edc9523ff931000a7290b1d477e76231226ef
tjioedava <tjioedava@gmail.com>
2024-08-23 23:33:36 +0700

    featuring register and login session

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path, reverse
-from django.http import HttpResponseRedirect;
+from django.urls import path
+from django.shortcuts import redirect
 from .views import *;
 
 urlpatterns = [
-    path('', lambda request: HttpResponseRedirect(reverse('home')), name='home-generic'),
+    path('', lambda request: redirect('home'), name='home-generic'),
     path('home/', show_home, name='home'),
     path('create-product/', create_product, name='create-product'),
     path('button-click/', button_click, name='button-click'),
     path('show-product/', show_product, name='show-product'),
     path('show-product/<int:id>/', show_product_by_id, name='show-product-by-id'),
+    path('register/', register, name='register'),
+    path('login/', log_in, name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404;
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
-from django.core import serializers;
+from django.core import serializers
+from django.shortcuts import redirect;
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .forms import ProductForm
 from .models import Product
 
+@login_required(login_url='/login/')
 def show_home(request):
     products = Product.objects.all() 
     return render(request, 'home.html', {'products': products})
 
+@login_required(login_url='/login/')
 def create_product(request):
 
     form = ProductForm(request.POST or None)
 
     if request.method == 'POST' and form.is_valid():
         form.save()
-        return HttpResponseRedirect(reverse('home'))
+        return redirect('home')
 
     return render(request, 'create-product.html', {'form': form})
 
+@login_required(login_url='/login/')
 def show_product(request):
 
     if request.method != 'GET':
@@ -34,6 +42,7 @@ def show_product(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize(format, data), content_type = type)
 
+@login_required(login_url='/login/')
 def show_product_by_id(request, id):
 
     if request.method != 'GET':
@@ -49,17 +58,49 @@ def show_product_by_id(request, id):
     data = Product.objects.filter(pk = id)
     return HttpResponse(serializers.serialize(format, data), content_type = type)
 
-def button_click(request):
+def register(request):
+
+    form = UserCreationForm()
 
     if request.method == 'POST':
-        data = request.POST
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            messages.info(request, 'The form is invalid')
+        
+    return render(request, 'register.html', {'form': form})
 
-        value = data.get('source')
+def log_in(request):
 
-        if value == 'clear':
-            Product.objects.all().delete()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username = username, password = password)
 
-        return HttpResponseRedirect(reverse('home'))
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(request.GET.get('next') or reverse('home'))
 
-    else:
+        else:
+            messages.info(request, 'Login failed')
+        
+    return render(request, 'login.html', dict())
+
+def button_click(request):
+
+    if request.method != 'POST':
         raise Http404
+    data = request.POST
+
+    action = data.get('source')
+
+    if action == 'clear':
+        Product.objects.all().delete()
+
+    if action == 'logout':
+        logout(request)
+        return redirect('login')
+
+    return redirect('home')
